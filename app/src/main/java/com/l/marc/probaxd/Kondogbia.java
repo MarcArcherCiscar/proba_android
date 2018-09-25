b5538639702384e05bb3b9900031a82c2c983eda
DESKTOP-64GBF8K\Marc <marcarcher1999@gmail.com>
2018-09-25 18:35:21 +0200

    Initial commit

---
+++ app/src/main/java/com/l/marc/probaxd/Kondogbia.java
@@ -0,0 +1,14 @@
+package com.l.marc.probaxd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kondogbia extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kondogbia);
+    }
+}
